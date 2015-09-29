623c1a972d12e95b6d6c7982e667bd4e7df58720
pysarevm <Mykola.Pysarev@barclayscapital.com>
2015-09-29 14:04:07 +0300

    Скрипт для выведения  sys.path

---
+++ sys_path.py
@@ -0,0 +1,7 @@
+__author__ = 'Pysarev'
+import sys
+print(sys.path)
+#sys.path.append("D:\GIT_Repository\python_homework\env2\Lib\site-packages")
+#print(sys.path)
+#sys.path.remove('c:\\users\\pysarev\\appdata\\local\\programs\\python\\python35-32\\Lib')
+print(sys.path)
