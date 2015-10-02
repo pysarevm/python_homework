2bba1d71c71037e1a53b2ffcb4eb6b0687d23922
pysarevm <Mykola.Pysarev@barclayscapital.com>
2015-10-02 12:03:15 +0300

    homework 3 - just in case

--- add_contact_test.py
+++ add_contact_test.py
@@ -10,6 +10,38 @@ def is_alert_present(wd):
     except:
         return False
 
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax,
+                 email, email2, email3, homepage, dropbox1_choise, dropbox2_choise, byear, dropbox3_choise,
+                 dropbox4_choise, ayear, address2, phone2, notes):
+        self.firstname=firstname
+        self.middlename=middlename
+        self.lastname=lastname
+        self.nickname=nickname
+        self.title=title
+        self.company=company
+        self.address=address
+        self.home=home
+        self.mobile=mobile
+        self.work=work
+        self.fax=fax
+        self.email=email
+        self.email2=email2
+        self.email3=email3
+        self.homepage=homepage
+        self.dropbox1_choise=dropbox1_choise
+        self.dropbox2_choise=dropbox2_choise
+        self.byear=byear
+        self.dropbox3_choise=dropbox3_choise
+        self.dropbox4_choise=dropbox4_choise
+        self.ayear=ayear
+        self.address2=address2
+        self.phone2=phone2
+        self.notes=notes
+
+
+
+
 class add_contact_test(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
@@ -19,7 +51,6 @@ class add_contact_test(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-
         self.add_contact_information(wd)
         self.logout(wd)
 
@@ -45,22 +76,12 @@ class add_contact_test(unittest.TestCase):
         self.fill_the_field(wd, field_name="email2", field_content="")
         self.fill_the_field(wd, field_name="email3", field_content="")
         self.fill_the_field(wd, field_name="homepage", field_content="www.google.com")
-        """if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[1]").is_selected():
-        wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[1]").click()"""
         self.select_from_dropbox(wd, box_number=1, choice_number=14)
         self.select_from_dropbox(wd, box_number=2, choice_number=6)
-        """if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[6]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[6]").click()"""
         self.fill_the_field(wd, field_name="byear", field_content="1982")
         self.select_from_dropbox(wd, box_number=3, choice_number=1)
-        """if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[1]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[1]").click()"""
         self.select_from_dropbox(wd, box_number=4, choice_number=1)
-        """if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[1]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[1]").click()"""
         self.fill_the_field(wd, field_name="ayear", field_content="")
-        """if not wd.find_element_by_xpath("//div[@id='content']/form/select[5]//option[1]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[5]//option[1]").click()"""
         self.fill_the_field(wd, field_name="address2", field_content="some address")
         self.fill_the_field(wd, field_name="phone2", field_content="0987654321")
         self.fill_the_field(wd, field_name="notes", field_content="")
