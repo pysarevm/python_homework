56a7f77d6bd007fd887c345f7819402019bf443c
pysarevm <Mykola.Pysarev@barclayscapital.com>
2015-10-02 14:18:41 +0300

    Homework 3 for checking

--- add_contact_test.py
+++ add_contact_test.py
@@ -39,8 +39,17 @@ class Contact:
         self.phone2=phone2
         self.notes=notes
 
-
-
+    def random_choise(self, dropbox_number):
+        if dropbox_number == 1:
+            return random.randint(1, 33)
+        elif dropbox_number == 2:
+            return random.randint(1, 13)
+        elif dropbox_number == 3:
+            return random.randint(1, 33)
+        elif dropbox_number == 4:
+            return random.randint(1, 13)
+        else:
+            print("There is no ", dropbox_number, " dropbox number available. Please, chose between 1, 2, 3, 4")
 
 class add_contact_test(unittest.TestCase):
     def setUp(self):
@@ -48,43 +57,46 @@ class add_contact_test(unittest.TestCase):
         self.wd.implicitly_wait(60)
 
     def test_add_contact(self):
+        contact1 = Contact("Mykola", "Volodymyrovych", "Pysarev", "NA", "engineer", "EPAM", "city. street, house",
+                           "0577778881", "0679876543", "", "", "mail@yandex.ua", "", "", "www.google.com",
+                           14, 6, "1982", 1, 1, "", "", "1234567890", "")
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.add_contact_information(wd)
+        self.add_contact_information(wd, contact1)
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Вийти").click()
 
-    def add_contact_information(self, wd):
+    def add_contact_information(self, wd, contact):
         # init contact creation
         self.navigate_add_contact_page(wd)
         # Filling contact information
-        self.fill_the_field(wd, field_name="firstname", field_content="Mykola")
-        self.fill_the_field(wd, field_name="middlename", field_content="Volodymyrovych")
-        self.fill_the_field(wd, field_name="lastname", field_content="Pysarev")
-        self.fill_the_field(wd, field_name="nickname", field_content="NA")
-        self.fill_the_field(wd, field_name="title", field_content="engineer")
-        self.fill_the_field(wd, field_name="company", field_content="EPAM")
-        self.fill_the_field(wd, field_name="address", field_content="city. street, house")
-        self.fill_the_field(wd, field_name="home", field_content="0577778881")
-        self.fill_the_field(wd, field_name="mobile", field_content="0679876543")
-        self.fill_the_field(wd, field_name="work", field_content="")
-        self.fill_the_field(wd, field_name="fax", field_content="")
-        self.fill_the_field(wd, field_name="email", field_content="mail@yandex.ua")
-        self.fill_the_field(wd, field_name="email2", field_content="")
-        self.fill_the_field(wd, field_name="email3", field_content="")
-        self.fill_the_field(wd, field_name="homepage", field_content="www.google.com")
-        self.select_from_dropbox(wd, box_number=1, choice_number=14)
-        self.select_from_dropbox(wd, box_number=2, choice_number=6)
-        self.fill_the_field(wd, field_name="byear", field_content="1982")
-        self.select_from_dropbox(wd, box_number=3, choice_number=1)
-        self.select_from_dropbox(wd, box_number=4, choice_number=1)
-        self.fill_the_field(wd, field_name="ayear", field_content="")
-        self.fill_the_field(wd, field_name="address2", field_content="some address")
-        self.fill_the_field(wd, field_name="phone2", field_content="0987654321")
-        self.fill_the_field(wd, field_name="notes", field_content="")
+        self.fill_the_field(wd, field_name="firstname", field_content=contact.firstname)
+        self.fill_the_field(wd, field_name="middlename", field_content=contact.middlename)
+        self.fill_the_field(wd, field_name="lastname", field_content=contact.lastname)
+        self.fill_the_field(wd, field_name="nickname", field_content=contact.nickname)
+        self.fill_the_field(wd, field_name="title", field_content=contact.title)
+        self.fill_the_field(wd, field_name="company", field_content=contact.company)
+        self.fill_the_field(wd, field_name="address", field_content=contact.address)
+        self.fill_the_field(wd, field_name="home", field_content=contact.home)
+        self.fill_the_field(wd, field_name="mobile", field_content=contact.mobile)
+        self.fill_the_field(wd, field_name="work", field_content=contact.work)
+        self.fill_the_field(wd, field_name="fax", field_content=contact.fax)
+        self.fill_the_field(wd, field_name="email", field_content=contact.email)
+        self.fill_the_field(wd, field_name="email2", field_content=contact.email2)
+        self.fill_the_field(wd, field_name="email3", field_content=contact.email3)
+        self.fill_the_field(wd, field_name="homepage", field_content=contact.homepage)
+        self.select_from_dropbox(wd, box_number=1, choice_number=contact.random_choise(1))
+        self.select_from_dropbox(wd, box_number=2, choice_number=contact.random_choise(2))
+        self.fill_the_field(wd, field_name="byear", field_content=contact.byear)
+        self.select_from_dropbox(wd, box_number=3, choice_number=contact.random_choise(3))
+        self.select_from_dropbox(wd, box_number=4, choice_number=contact.random_choise(4))
+        self.fill_the_field(wd, field_name="ayear", field_content=contact.ayear)
+        self.fill_the_field(wd, field_name="address2", field_content=contact.address2)
+        self.fill_the_field(wd, field_name="phone2", field_content=contact.phone2)
+        self.fill_the_field(wd, field_name="notes", field_content=contact.notes)
         # Submit contact
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
