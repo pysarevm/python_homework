b3d1024a684696b3f1ce3c041d3504968d8e35f7
pysarevm <Mykola.Pysarev@barclayscapital.com>
2015-10-15 20:00:32 +0300

    del_group test modified

--- test/del_group_test.py
+++ test/del_group_test.py
@@ -1,4 +1,7 @@
+from model.group import Group
 __author__ = 'Pysarev'
 
 def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="delete_test"))
     app.group.delete_first_group()
