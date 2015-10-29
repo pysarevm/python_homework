4b2c2118e996a843393dba04ed6b507f78a52cc3
pysarevm <Mykola.Pysarev@barclayscapital.com>
2015-10-29 18:03:13 +0200

    after video 6_4

--- test/add_group_test.py
+++ test/add_group_test.py
@@ -1,19 +1,8 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
+from data.groups import constant as testdata
 
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
-
-
-testdata = [Group("", "", "")]+[
-    Group(name=random_string("Name", 10), header=random_string("Header", 20), footer=random_string("Footer", 20))
-    for i in range(3)
-]
 
 @pytest.mark.parametrize("group_parameters", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group_parameters):
