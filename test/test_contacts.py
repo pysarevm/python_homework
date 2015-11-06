422fd74d94fd5220e6b40939f588040542174edd
pysarevm <Mykola.Pysarev@barclayscapital.com>
2015-11-06 18:15:43 +0200

    Homework N21

--- test/test_contacts.py
+++ test/test_contacts.py
@@ -1,14 +1,33 @@
 import random
+from model.contact import Contact
 
 
-def test_phones_on_home_page(app):
-    contacts_quantity = len(app.contact.get_contact_list())
-    contact_number = random.randrange(contacts_quantity)
-    contact_from_home_page = app.contact.get_contact_list()[contact_number]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(contact_number)
-    print (contact_from_edit_page, contact_from_home_page)
-    assert (contact_from_home_page.firstname == contact_from_edit_page.firstname and
-        contact_from_home_page.lastname == contact_from_edit_page.lastname and
-        contact_from_home_page.address == contact_from_edit_page.address and
-        contact_from_home_page.all_emails == app.contact.merge_emails_like_on_home_page(contact_from_edit_page) and
-        contact_from_home_page.all_phones_from_home_page == app.contact.merge_phones_like_on_home_page(contact_from_edit_page))
+def test_contacts_on_home_page(app, db):
+    contacts_from_home_page = app.contact.get_contact_list()
+    contacts_from_db = db.get_contact_list()
+    assert len(contacts_from_home_page) == len(contacts_from_db)
+    for contact in contacts_from_db:
+        contact.all_phones_from_home_page = app.contact.merge_phones_like_on_home_page(contact)
+        contact.all_emails = app.contact.merge_emails_like_on_home_page(contact)
+        if contact.firstname:
+            contact.firstname= contact.firstname.strip()
+        else:
+            contact.firstname = ''
+        if contact.lastname:
+            contact.lastname= contact.lastname.strip()
+        else:
+            contact.lastname = ''
+        if contact.address:
+            contact.address = contact.address.strip()
+        else:
+            contact.address = ''
+    print("from home page: ", contacts_from_home_page)
+    print("from DB: ", contacts_from_db)
+    sorted_contacts_from_home_page = sorted(contacts_from_home_page, key=Contact.id_or_max)
+    sorted_contacts_from_db = sorted(contacts_from_db,  key=Contact.id_or_max)
+    for i in range(0, len(contacts_from_db)-1):
+        print("iteration: ",i)
+        assert (sorted_contacts_from_home_page[i] == sorted_contacts_from_db[i] and
+                sorted_contacts_from_home_page[i].address == sorted_contacts_from_db[i].address and
+                sorted_contacts_from_home_page[i].all_phones_from_home_page == sorted_contacts_from_db[i].all_phones_from_home_page and
+                sorted_contacts_from_home_page[i].all_emails == sorted_contacts_from_db[i].all_emails)
